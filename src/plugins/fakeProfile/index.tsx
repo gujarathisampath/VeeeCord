ef2b1296ab0e425facf380778a8fedfd0362c036
Sampath Gujarathi <gujarathi.sampath@gmail.com>
2024-11-09 19:41:20 +0530

    Fixed 3y3 button

--- src/plugins/fakeProfile/index.tsx
+++ src/plugins/fakeProfile/index.tsx
@@ -373,9 +373,9 @@ export default definePlugin({
             }
         },
         {
-            find: ".USER_SETTINGS_RESET_PROFILE_THEME",
+            find: "#{intl::USER_SETTINGS_RESET_PROFILE_THEME}",
             replacement: {
-                match: /RESET_PROFILE_THEME}\)(?<=color:(\i),.{0,500}?color:(\i),.{0,500}?)/,
+                match: /#{intl::USER_SETTINGS_RESET_PROFILE_THEME}\)}\)(?<=color:(\i),.{0,500}?color:(\i),.{0,500}?)/,
                 replace: "$&,$self.addCopy3y3Button({primary:$1,accent:$2})"
             }
         },
