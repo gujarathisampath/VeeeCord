86b35744e1b2eebe3cdefd47e6addcb815abf938
Sampath Gujarathi <gujarathi.sampath@gmail.com>
2025-07-11 19:00:13 +0530

    fix(constants): update version from v2.7 to v3.0.0

--- src/plugins/fakeProfile/lib/constants.ts
+++ src/plugins/fakeProfile/lib/constants.ts
@@ -10,5 +10,5 @@ export const SKU_ID_DISCORD = "100101099222222";
 export const SKU_ID = "100101099222224";
 export const GUILD_ID = "1117373291095662623";
 export const INVITE_KEY = "ffmkewQ4R7";
-export const VERSION = "v2.7";
+export const VERSION = "v3.0.0";
 export const FETCH_COOLDOWN = 1000 * 60 * 60 * 1;
